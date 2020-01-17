015624c986a93fba68185fe1211a31e584d5a1b0
Duncan Hall <duncandhall@gmail.com>
2020-01-17 12:55:13 -0500

    done with array_pair_sum solutions

--- problems/array-pair-sum/my_array_pair_sum.py
+++ problems/array-pair-sum/my_array_pair_sum.py
@@ -1,6 +1,7 @@
 #  Given an integer array, output all pairs that sum up to a specific value k. Consider the fact that the same number can add up to `k` with its duplicates in the array.
 #
 #  > For example the array is [1, 1, 2, 3, 4] and the desired sum is 4. Should we output the pair (1, 3) twice or just once? Also do we output the reverse of a pair, meaning both (3, 1) and (1, 3)? Let’s keep the output as short as possible and print each pair only once. So, we will output only one copy of (1, 3). Also note that we shouldn’t output (2, 2) because it’s not a pair of two distinct elements.
+# [EDIT] the above clarification invalidates the examples below
 #
 #  ## Example
 #
@@ -9,12 +10,34 @@
 #  f(8, [3, 4, 5, 4, 4]) // [ [3, 5], [4, 4], [4, 4], [4, 4] ]
 #  ```
 
+from copy import deepcopy
 
+
+# first solution is O(n^2)
 def sol1(k, q):
-    qq = []
+    s = set()
     for i, n in enumerate(q):
         for j in range(i+1, len(q)):
             if q[i] + q[j] == k:
-                qq.append([q[i], q[j]])
-    return qq
+                s.add((q[i], q[j]))
+    res = [list(li) for li in s]
+    return res
 
+
+# this is a better solution using sort then linear time pairing
+def sol2(k, q):
+    q = deepcopy(q)
+    res = []
+
+    q.sort()
+    i = 0
+    j = len(q) - 1
+    while i < j:
+        if q[i] + q[j] == k:
+            res.append([q[i], q[j]])
+        if q[i] + q[j] <= k:
+            i += 1
+        if q[i] + q[j] >= k:
+            j -= 1
+
+    return res
--- problems/array-pair-sum/test_array_pair_sum.py
+++ problems/array-pair-sum/test_array_pair_sum.py
@@ -1,12 +1,14 @@
 import unittest
-from array_pair_sum import *
+from my_array_pair_sum import *
+
+from copy import deepcopy
 
 
 class TestArrayPairSum(unittest.TestCase):
 
     def test_answer_tester(self):
         self.assertTrue(self.answer_tester(
-            [[6, 4], [7, 3], [6, 4]]
+            [[6, 4], [7, 3], [6, 4]],
             [[7, 3], [4, 6], [4, 6]]
         ))
         self.assertTrue(self.answer_tester(
@@ -15,28 +17,41 @@ class TestArrayPairSum(unittest.TestCase):
         ))
 
     def test_sol1(self):
-        self.assertEqual(
+        self.assertTrue(self.answer_tester(
                 [ [6, 4], [7, 3] ],
                 sol1(10, [3, 4, 5, 6, 7])
-        )
-        self.assertEqual(
-                [ [3, 5], [4, 4], [4, 4], [4, 4] ],
+        ))
+        self.assertTrue(self.answer_tester(
+                [ [3, 5], [4, 4] ],
                 sol1(8, [3, 4, 5, 4, 4])
-        )
+        ))
 
+    def test_sol2(self):
+        self.assertTrue(self.answer_tester(
+                [ [6, 4], [7, 3] ],
+                sol2(10, [3, 4, 5, 6, 7])
+        ))
+        self.assertTrue(self.answer_tester(
+                [ [3, 5], [4, 4] ],
+                sol2(8, [3, 4, 5, 4, 4])
+        ))
+
+    # for our purposes, sorting in O(n*log(n)) is fine
+    # for our purposes, sorting in O(n*log(n)) is fine
     def answer_tester(self, a, b):
         #  breakpoint()
-        b_set = set([tuple(bb) for bb in b])
-        for item in a:
-            if tuple(item) in b_set:
-                b_set.discard(tuple(item))
-                continue
-            elif tuple(reversed(item)) in b_set:
-                b_set.discard(tuple(reversed(item)))
-                continue
-            else:
-                return False
-        return True
+        aa = deepcopy(a)
+        bb = deepcopy(b)
+        for l in [aa, bb]:
+            for li in l:
+                li.sort()
+            l.sort()
+        if aa != bb:
+            print("failed answer test")
+            print("expected: ", a)
+            print("got:      ", b)
+        return aa == bb
+
 
 
 if __name__ == '__main__':
